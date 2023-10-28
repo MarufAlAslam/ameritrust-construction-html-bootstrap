0b1a6f2af11e84cf6bd3a5563bf7d5bde3d6fb7a
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-10-28 22:44:36 +0600

    cards added

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slideToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button type="button" class="prev-btn btn"><i class="fa fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="next-btn btn"><i class="fa fa-chevron-right"></i></button>',
+});
